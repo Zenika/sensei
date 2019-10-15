adce2b15264342f3ac26e877cbeecc5820443dae
Hugo Wood <hx4.5@free.fr>
2019-10-16 00:35:47 +0200

    :truck: rename run.js to slides.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/run.js",
+  entry: "./src/slides.js",
   module: {
     rules: [
       {
