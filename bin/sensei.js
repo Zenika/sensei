f1e92959b6b614f57a43084e0206326ce2af1b88
Martin Choraine <martin.choraine@gmail.com>
2020-09-24 18:54:27 +0200

    🎨 update sensei CLI from PR recommandations

--- bin/sensei.js
+++ bin/sensei.js
@@ -10,8 +10,6 @@ function cli(args) {
   let trainingMaterialFolder = args[3];
   switch (args[2]) {
     case "serve":
-    case null:
-    case undefined:
       serve(trainingMaterialFolder);
       break;
     case "build":
@@ -31,7 +29,7 @@ function cli(args) {
 function serve(trainingMaterialFolder = ".") {
   console.log("Start dev server");
   const server = new WebpackDevServer(
-    webpack(config({ material: trainingMaterialFolder }))
+    webpack(config({material: trainingMaterialFolder}))
   );
   server.listen(8080, "localhost", function (err) {
     if (err) {
@@ -47,7 +45,7 @@ function serve(trainingMaterialFolder = ".") {
 function build(trainingMaterialFolder = ".") {
   console.log("Build slides & labs");
   return new Promise((resolve, reject) => {
-    webpack(config({ material: trainingMaterialFolder }), (err, stats) => {
+    webpack(config({material: trainingMaterialFolder}), (err, stats) => {
       if (err || stats.hasErrors()) {
         console.error("HTML files generation failed!", err);
         reject();
@@ -59,66 +57,74 @@ function build(trainingMaterialFolder = ".") {
   });
 }
 
-function pdf(trainingMaterialFolder = ".") {
+async function pdf(trainingMaterialFolder = ".") {
   console.log("Generate pdf slides & labs");
-  build(trainingMaterialFolder).then(() => {
-    pdfSlides(path.basename(trainingMaterialFolder));
-    pdfLabs(path.basename(trainingMaterialFolder));
-  });
+  await build(trainingMaterialFolder);
+  pdfSlides(path.basename(trainingMaterialFolder));
+  pdfLabs(path.basename(trainingMaterialFolder));
 }
 
 function help() {
-  console.log("Sensei training material build tool");
-  console.log("");
-  console.log("Commands :");
-  console.log("  help| -h         display help");
   console.log(
-    "  serve <path>     serve slides & labs for path [default: current directory]"
-  );
-  console.log(
-    "  build <path>     build slides & labs for path [default: current directory]"
-  );
-  console.log(
-    "  pdf <path>       generate pdf slides & labs for path [default: current directory]"
-  );
+    `Sensei training material build tool
+
+    Commands:
+      help | -h        display help
+      serve <path>     serve slides & labs for path [default: current directory]
+      build <path>     build slides & labs for path [default: current directory]
+      pdf <path>       generate pdf slides & labs for path [default: current directory]
+  `);
 }
 
 function pdfSlides(trainingName) {
-  const child = spawn(
-    path.resolve(path.join(__dirname, "../src/pdf/node_modules/.bin/decktape")),
-    [
-      "reveal",
-      "-p",
-      "0",
-      "--size",
-      "1420x800",
-      "./dist/slides.html",
-      `./pdf/Zenika-Formation-${trainingName}-Slides.pdf`,
-    ]
-  );
+  return new Promise((resolve, reject) => {
+    const child = spawn(
+      path.resolve(path.join(__dirname, "../src/pdf/node_modules/.bin/decktape")),
+      [
+        "reveal",
+        "-p",
+        "0",
+        "--size",
+        "1420x800",
+        "./dist/slides.html",
+        `./pdf/Zenika-Formation-${trainingName}-Slides.pdf`,
+      ]
+    );
 
-  child.stdout.on("data", function (data) {
-    process.stdout.write(data);
-  });
+    child.stdout.on("data", function (data) {
+      process.stdout.write(data);
+    });
 
-  child.on("error", function (data) {
-    console.error("PDF slides generation failed !", data);
-  });
+    child.on("exit", function () {
+      console.log("PDF slides generated");
+      resolve();
+    });
+
+    child.on("error", function (err) {
+      console.error("PDF slides generation failed!", err);
+      reject(err)
+    });
+
+  })
 }
 
 function pdfLabs(trainingName) {
-  const child = fork(path.resolve(path.join(__dirname, "../src/pdf/pdf.js")), [
-    `file:${path.resolve("./dist/labs.html")}`,
-    `./pdf/Zenika-Formation-${trainingName}-CahierExercices.pdf`,
-  ]);
+  return new Promise((resolve, reject) => {
+    const child = fork(path.resolve(path.join(__dirname, "../src/pdf/pdf.js")), [
+      `file:${path.resolve("./dist/labs.html")}`,
+      `./pdf/Zenika-Formation-${trainingName}-CahierExercices.pdf`,
+    ]);
 
-  child.on("exit", function () {
-    console.log("PDF labs generated");
-  });
+    child.on("exit", function () {
+      console.log("PDF labs generated");
+      resolve()
+    });
 
-  child.on("error", function (data) {
-    console.error("PDF Labs generation failed !", data);
-  });
+    child.on("error", function (err) {
+      console.error("PDF Labs generation failed!", err);
+      reject(err)
+    });
+  })
 }
 
 cli(process.argv);
