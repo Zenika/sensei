e7d5bf3b2082110768cb148b684c2547593de118
Hugo Wood <hugo.wood@zenika.com>
2020-09-28 15:30:03 +0200

    💄 add slide numbers and copyright (#34)

--- src/loaders/slides-json-loader.js
+++ src/loaders/slides-json-loader.js
@@ -1,6 +1,8 @@
 module.exports = function (content) {
   const json = JSON.parse(content);
-  const imports = json.map((f, i) => `import f${i} from "./${f}";`).join("\n");
+  const imports = json
+    .map((f, i) => `import f${i} from "./${f}?chapterIndex=${i}";`)
+    .join("\n");
   return `${imports}\nexport default [${json
     .map((f, i) => `f${i}`)
     .join(", ")}]`.trim();
