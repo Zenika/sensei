4543b0c364db32a6df405bb44960adba00261b00
Eric <eric.briand@gmail.com>
2020-10-01 00:26:32 +0200

    :lipstick: fix prettier errors

--- bin/sensei.js
+++ bin/sensei.js
@@ -134,20 +134,21 @@ function pdfLabs(trainingName) {
 }
 
 // quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint () {
+process.on("SIGINT", function onSigint() {
   console.info(
-    'Got SIGINT (aka ctrl-c in docker). Graceful shutdown',
+    "Got SIGINT (aka ctrl-c in docker). Graceful shutdown",
     new Date().toISOString()
   );
   process.exit();
 });
 
 // quit properly on docker stop
-process.on('SIGTERM', function onSigterm () {
-  console.info('Got SIGTERM (docker container stop). Graceful shutdown',
+process.on("SIGTERM", function onSigterm() {
+  console.info(
+    "Got SIGTERM (docker container stop). Graceful shutdown",
     new Date().toISOString()
   );
   process.exit();
-})
+});
 
 cli(process.argv);
