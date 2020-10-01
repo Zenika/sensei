37b478519e8dbee9562ac24fcd3a898e23859ce1
Eric Briand <1011902+ebriand@users.noreply.github.com>
2020-10-01 17:39:15 +0200

    🐳  fix ctrl+c in docker (#41)
    Closes #38.

--- bin/sensei.js
+++ bin/sensei.js
@@ -133,4 +133,22 @@ function pdfLabs(trainingName) {
   });
 }
 
+// quit on ctrl-c when running docker in terminal
+process.on("SIGINT", function onSigint() {
+  console.info(
+    "Got SIGINT (aka ctrl-c in docker). Graceful shutdown",
+    new Date().toISOString()
+  );
+  process.exit();
+});
+
+// quit properly on docker stop
+process.on("SIGTERM", function onSigterm() {
+  console.info(
+    "Got SIGTERM (docker container stop). Graceful shutdown",
+    new Date().toISOString()
+  );
+  process.exit();
+});
+
 cli(process.argv);
