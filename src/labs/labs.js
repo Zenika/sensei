c68239151a1013ea5d11e0ea3094b3a8c93174e6
Nicolas MOREAU <moreau-nicolas@users.noreply.github.com>
2021-01-06 11:11:28 +0100

    :sparkles: Add version information in Labs
    Add a `span` with class "version" to the first `div` of the Labs handbook.
    Add CSS style for this `span`.
    
    Fixes #53

--- src/labs/labs.js
+++ src/labs/labs.js
@@ -4,3 +4,11 @@ import labs from "training-material/CahierExercices/parts.json";
 
 const labsContainer = document.querySelector(".labs");
 labsContainer.innerHTML = labs.join("\n");
+
+let coverPageContainer = labsContainer.querySelector(":root div:first-of-type");
+
+let versionSpan = document.createElement("span");
+versionSpan.className = 'version';
+versionSpan.innerHTML = MATERIAL_VERSION;
+
+coverPageContainer.appendChild(versionSpan);
