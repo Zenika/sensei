ee9f006a0241678500f7a5569b0472fc132ada6a
Nicolas MOREAU <moreau-nicolas@users.noreply.github.com>
2021-01-06 11:52:51 +0100

    :sparkles: Update labs titles

--- src/labs/labs.js
+++ src/labs/labs.js
@@ -1,6 +1,7 @@
 import "prismjs/themes/prism.css";
 import "./labs.css";
 import labs from "training-material/CahierExercices/parts.json";
+import slidesContent from "training-material/Slides/slides.json";
 
 const labsContainer = document.querySelector(".labs");
 labsContainer.innerHTML = labs.join("\n");
@@ -12,3 +13,12 @@ versionSpan.className = "version";
 versionSpan.innerHTML = MATERIAL_VERSION;
 
 coverPageContainer.appendChild(versionSpan);
+
+const convertTextToHtml = document.createElement("div");
+convertTextToHtml.innerHTML = slidesContent[0];
+
+const trainingTitle = convertTextToHtml.querySelector("section h1:first-of-type").textContent;
+
+document.title = trainingTitle + " - Labs 👩‍🔬";
+const coverPageTitle = labsContainer.querySelector(":root div:first-of-type h1:first-of-type");
+coverPageTitle.textContent = trainingTitle;
