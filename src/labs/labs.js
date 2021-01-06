fb1d0c6cf6e4f0e3b7670e16b7e06ed09ff87509
Nicolas MOREAU <moreau-nicolas@users.noreply.github.com>
2021-01-06 12:04:20 +0100

    :art: prettier

--- src/labs/labs.js
+++ src/labs/labs.js
@@ -8,7 +8,7 @@ labsContainer.innerHTML = labs.join("\n");
 let coverPageContainer = labsContainer.querySelector(":root div:first-of-type");
 
 let versionSpan = document.createElement("span");
-versionSpan.className = 'version';
+versionSpan.className = "version";
 versionSpan.innerHTML = MATERIAL_VERSION;
 
 coverPageContainer.appendChild(versionSpan);
