1d262c7e29e73adbbfed68fa33cbcc7689f563aa
Nicolas MOREAU <moreau-nicolas@users.noreply.github.com>
2021-01-06 11:53:14 +0100

    :sparkles: Update index titles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./index.css";
+import slidesContent from "training-material/Slides/slides.json";
+
+const convertTextToHtml = document.createElement("div");
+convertTextToHtml.innerHTML = slidesContent[0];
+
+const trainingTitle = convertTextToHtml.querySelector("section h1:first-of-type").textContent;
+
+document.title = trainingTitle + " 🎓";
+const pageTitle = document.querySelector(":root div:first-of-type h1:first-of-type");
+pageTitle.textContent = trainingTitle;
