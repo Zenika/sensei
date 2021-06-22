36e8f5d949af679f834eb12ae878221e89906a24
Hugo Wood <hugo.wood@zenika.com>
2021-06-22 10:59:21 +0200

    error instead of warning

--- src/title.js
+++ src/title.js
@@ -11,7 +11,7 @@ const trainingTitle =
   (trainingTitleElement && trainingTitleElement.textContent) ||
   "TITLE NOT FOUND (see browser console)";
 if (!trainingTitleElement) {
-  console.warn(
+  console.error(
     "The title of the training was not found. Please ensure that the slides include a first level title (e.g. '# My title' or '<h1>My title</h1>') in the first file referenced in 'Slides/slides.json'."
   );
 }
