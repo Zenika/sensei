21284fed5e428004a5132f27ae03ba5c7c9abcb7
Hugo Wood <hugo.wood@zenika.com>
2021-06-22 10:58:29 +0200

    add warning with more details

--- src/title.js
+++ src/title.js
@@ -9,6 +9,11 @@ const trainingTitleElement = convertTextToHtml.querySelector(
 
 const trainingTitle =
   (trainingTitleElement && trainingTitleElement.textContent) ||
-  "{Training Title}";
+  "TITLE NOT FOUND (see browser console)";
+if (!trainingTitleElement) {
+  console.warn(
+    "The title of the training was not found. Please ensure that the slides include a first level title (e.g. '# My title' or '<h1>My title</h1>') in the first file referenced in 'Slides/slides.json'."
+  );
+}
 
 export { trainingTitle };
