b3e11428940e51fa873cae2afd55e700edf6cf01
Hugo Wood <hugo.wood@zenika.com>
2021-06-22 11:42:19 +0200

    🐛 fix crash when title not fond (#98)

--- src/title.js
+++ src/title.js
@@ -3,8 +3,17 @@ import slidesContent from "training-material/Slides/slides.json";
 const convertTextToHtml = document.createElement("div");
 convertTextToHtml.innerHTML = slidesContent[0];
 
-const trainingTitle = convertTextToHtml.querySelector(
+const trainingTitleElement = convertTextToHtml.querySelector(
   "section h1:first-of-type"
-).textContent;
+);
+
+const trainingTitle =
+  (trainingTitleElement && trainingTitleElement.textContent) ||
+  "TITLE NOT FOUND (see browser console)";
+if (!trainingTitleElement) {
+  console.error(
+    "The title of the training was not found. Please ensure that the slides include a first level title (e.g. '# My title' or '<h1>My title</h1>') in the first file referenced in 'Slides/slides.json'."
+  );
+}
 
 export { trainingTitle };
