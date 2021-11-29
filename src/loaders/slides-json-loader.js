e525bdb8b2d65d75245c0e0314efd7d4dd544866
Hugo Wood <hugo.wood@zenika.com>
2021-11-29 10:39:03 +0100

    add slides-json tests

--- src/loaders/slides-json-loader.js
+++ src/loaders/slides-json-loader.js
@@ -1,14 +1,9 @@
 module.exports = function (content) {
   const json = JSON.parse(content);
-  const imports = json
-    .map(
-      (filename, index) =>
-        `
-          import content${index} from "./${filename}?chapterIndex=${index}";
-          import title${index} from "./${filename}?titleOnly";
-        `
-    )
-    .join("\n");
+  const imports = json.flatMap((filename, index) => [
+    `import content${index} from "./${filename}?chapterIndex=${index}";`,
+    `import title${index} from "./${filename}?titleOnly";`,
+  ]);
   const chapters = json
     .map(
       (filename, index) =>
@@ -17,11 +12,11 @@ module.exports = function (content) {
     .join(",");
   const fullContent =
     json.map((filename, index) => `content${index}`).join(" + ") || '""';
-  return `
-    ${imports}
-
-    export const chapters = [${chapters}];
-    export const title = chapters[0]?.title || "";
-    export const content = ${fullContent};
-  `;
+  const lines = [
+    ...imports,
+    `export const chapters = [${chapters}];`,
+    `export const title = chapters[0]?.title || "";`,
+    `export const content = ${fullContent};`,
+  ];
+  return lines.join("\n");
 };
