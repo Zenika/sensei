2ac7f025bd7fe220e96c25bd46dc7babb60a0ba7
Alexandre Garnier <alexandre.garnier@zenika.com>
2022-11-02 11:07:36 +0100

    Use same test syntax for all loader tests
    Follow-up of #158 on new tests

--- src/build/loaders/workbook-toc-loader.test.js
+++ src/build/loaders/workbook-toc-loader.test.js
@@ -1,34 +1,23 @@
-const assert = require("assert");
+const assert = require("node:assert");
+const test = require("node:test");
 const workbookTocLoader = require("./workbook-toc-loader");
 
-{
+test("workbook TOC loader includes table of content at the marker", () => {
   const actual = workbookTocLoader("<!-- toc -->");
   const expected = /^<ul class="toc">/;
-  assert.match(
-    actual,
-    expected,
-    "workbook TOC loader does not include table of content at the marker"
-  );
-}
+  assert.match(actual, expected);
+});
 
-{
+test("workbook TOC loader includes table of content at every marker", () => {
   const actual = workbookTocLoader("<!-- toc --> space <!-- toc -->");
   const expected = /^<ul class="toc">.* space <ul class="toc">/;
-  assert.match(
-    actual,
-    expected,
-    "workbook TOC loader does not include table of content at every marker"
-  );
-}
+  assert.match(actual, expected);
+});
 
-{
+test("workbook TOC loader includes title in table of content", () => {
   const actual = workbookTocLoader(
     '<!-- toc --><h2 id="just-a-title">just a title</h2>'
   );
   const expected = /<li><a href="#just-a-title">just a title<\/a><\/li>/;
-  assert.match(
-    actual,
-    expected,
-    "workbook TOC loader does not include title in table of content"
-  );
-}
+  assert.match(actual, expected);
+});
