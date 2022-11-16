fe9f0d8442e9326242f7ff489fcebf2528d845d6
Alexandre Garnier <zigarn@users.noreply.github.com>
2022-11-16 18:14:36 +0100

    ✅ Use same test syntax for all loader tests (#171)
    Follow-up of #158 on new tests

--- src/build/loaders/workbook-json-loader.test.js
+++ src/build/loaders/workbook-json-loader.test.js
@@ -1,26 +1,19 @@
-const assert = require("assert");
+const assert = require("node:assert");
+const test = require("node:test");
 const { compileWorkbookJson } = require("./workbook-json-loader");
 
-(async () => {
+test("workbook.json loader can compile a workbook with one part", async () => {
   const actual = await compileWorkbookJson('["part1"]', {
     importWorkbookPart: () => "content",
   });
   const expected = "content";
-  assert.equal(
-    actual,
-    expected,
-    "workbook.json loader cannot compile a workbook with one part"
-  );
-})();
+  assert.equal(actual, expected);
+});
 
-(async () => {
+test("workbook.json loader can compile a workbook with two parts", async () => {
   const actual = await compileWorkbookJson('["part1", "part2"]', {
     importWorkbookPart: (filename) => filename,
   });
   const expected = "part1\npart2";
-  assert.equal(
-    actual,
-    expected,
-    "workbook.json loader cannot compile a workbook with two parts"
-  );
-})();
+  assert.equal(actual, expected);
+});
